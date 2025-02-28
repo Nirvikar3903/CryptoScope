085d080528b6992afdae8d5b5652eaf9cdfb7c06
Nirvikar3903 <nirvikar03.k@gmail.com>
2025-02-28 15:16:27 +0530

    Completed Home page with making it responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,54 @@
-import React from 'react'
+import React, { useContext } from "react";
 import { FaArrowRight } from "react-icons/fa";
-import logo from '../../assets/logo.png.png'
-import '../Navbar/Navbar.css'
-
+import logo from "../../assets/logo.png.png";
+import "../Navbar/Navbar.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default : {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
+
   return (
-    <div className='navbar'>
-    <img src={logo} alt='' className='logo'/>
-    <ul>
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <ul>
         <li>Home</li>
         <li>Feature</li>
         <li>Pricing</li>
         <li>Blog</li>
-    </ul>
-    <div className="nav-right">
-        <select>
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-            <option value="inr">INR</option>
+      </ul>
+      <div className="nav-right">
+        <select onChange={currencyHandler}>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
         </select>
-        <button>Sign up <FaArrowRight /> </button>
+        <button>
+          Sign up <FaArrowRight />{" "}
+        </button>
+      </div>
     </div>
+  );
+};
 
-</div>
-
-  )
-}
-
-export default Navbar
+export default Navbar;
--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
   export const CoinContext = createContext();
 
   const CoinContextProvider = (props) =>{
     // data will be received in form of array and saved in allCoin
     const [allCoin , setAllCoin] = useState([]);
-    const [currency ,setCurrency] = ({
+    const [currency ,setCurrency] = useState({
         name:"usd",
         symbol:"$",
     })
@@ -16,14 +16,21 @@ import { createContext, useState } from "react";
             headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-GsuRohwUhBknGbA5ub7sEeUd'}
           };
           
-          fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
+          fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
             .then(res => res.json())
-            .then(res => console.log(res))
+            .then(res => setAllCoin(res))
             .catch(err => console.error(err));
-    }
+          
+          }
+
+        useEffect(()=>{
+          fetchAllCoin();
+        },[currency])
+
+        // whenever currency dependency changes it will call the function and add another api request with updated currency name ${currency.name}
 
     const contextValue = {
-
+      allCoin , currency , setCurrency
     }
 
     return(
